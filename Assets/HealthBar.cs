f97a0b696cbf721cc2c2590a3d84956f8ac6c6f6
Andrea.P <andrea.ponzio@telecom-sudparis.eu>
2020-11-14 21:53:35 +0100

    Ajout barre de vie

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Slider healthBarSlider = null;
+
+    public GameObject playerShip;
+    private PlayerShipController playerShipController;
+
+    void Awake()
+    {
+        healthBarSlider = this.gameObject.GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        playerShipController = playerShip.GetComponent<PlayerShipController>();
+    }
+
+    void Update()
+    {
+        healthBarSlider.value = playerShipController.health;
+    }
+}

ba89272f346a165be0b9ad2c970fc72b655b571a
Matheo <matheo.anquetil@free.fr>
2020-11-15 13:48:20 +0100

    lol

--- Assets/Scripts/EnemyAimer.cs
+++ Assets/Scripts/EnemyAimer.cs
@@ -14,14 +14,15 @@ public class EnemyAimer : EnemyShip
 
     protected void FacePlayer()
     {
+        if (GameController.instance)
+        {
+            Vector2 player_direction = (Vector2)(GameController.instance.playerObject.transform.position - transform.position);
+            float angle = -Vector2.SignedAngle(player_direction, transform.up);
 
-        Vector2 player_direction = (Vector2)(GameController.instance.playerObject.transform.position - transform.position);
-        float angle = -Vector2.SignedAngle(player_direction, transform.up);
+            transform.Rotate(new Vector3(0, 0, angle) * aimingSpeed * Time.deltaTime);
 
-        transform.Rotate(new Vector3(0,0,angle)*aimingSpeed*Time.deltaTime);
-            
-        //rBody.SetRotation(Mathf.Lerp(transform.rotation, transform.rotation + angle, Time.fixedDeltaTime * rotationSpeed));
-
+            //rBody.SetRotation(Mathf.Lerp(transform.rotation, transform.rotation + angle, Time.fixedDeltaTime * rotationSpeed));
+        }
     }
 
 }
